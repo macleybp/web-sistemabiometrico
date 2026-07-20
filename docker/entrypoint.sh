fe81dcb4d6f6bc85f415c9e9e4d8ead66446ac37
macle <macleybar@gmail.com>
2026-07-19 23:51:53 -0500

    fix edit name file entry

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,4 @@ rm -f /etc/apache2/mods-enabled/mpm_event.*
 rm -f /etc/apache2/mods-enabled/mpm_worker.*
 a2enmod mpm_prefork 2>/dev/null || true
 
-exec /usr/local/bin/docker-entrypoint.sh "$@"
+exec /usr/local/bin/docker-php-entrypoint "$@"
